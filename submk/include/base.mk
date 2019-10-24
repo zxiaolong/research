ed3bcc679e4b104d5250a72ce3c1cbe7592a319a
Your Name <you@example.com>
2019-10-24 06:33:01 -0700

    update submk

--- submk/include/base.mk
+++ submk/include/base.mk
@@ -1,1 +1,11 @@
+
+# top dir of base.mk
 TOP_DIR=$(dir $(lastword $(MAKEFILE_LIST)))
+
+# toolchain dir
+TOOLCHAIN_DIR=/opt/toolchain/gcc
+
+# checking toolchain
+ifeq ("$(wildcard $(TOOLCHAIN_DIR))","")
+$(error "ERROR: $(TOOLCHAIN_DIR) not exist!")
+endif
